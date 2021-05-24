73a76ad65d7786c37f87dfd9874073c35d12fd68
world970511 <57740138+world970511@users.noreply.github.com>
2021-05-24 22:05:00 +0900

    소개글 크롤링
    >>>수정사항
    
    *셀레니움으로만 크롤링 하다가 bs4도 활용. 소개글을 읽어올 때 전체 html을 가져와 특정 부분을 추출하는 방식으로 바꿈(bs4고 셀레니움이고 추출이 잘 안됨)
    
    * 긍정(평점 4/5)- 1, 애매모호(평점 3)-0, 부정(평점 1/2)- -1로 나눔
    
    >>> 추가 수정예정
    
    * 생각보다 부정이 너무 적게 나와서 추가적으로 데이터를 더 수집하던가 0으로 둔 것도 이벤트 글을 제하고 부정으로 변경하던가 할 예정. 적어도 긍/부정 두 개 합쳐서 20000개의 학습 데이터를 만들고 싶은데(사실 더 뽑고 싶음....근데 그럼 내가 죽을 듯)

--- crawler_ code/RIDI_crawler_code.py
+++ crawler_ code/RIDI_crawler_code.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
 import time
 
+#메인 홈페이지에서 자동 로그인
 driver = webdriver.Chrome(r'.\chromedriver.exe')
 driver.get('https://ridibooks.com/account/login?return_url=https%3A%2F%2Fridibooks.com%2Fromance%2F')
 time.sleep(3)
 
+#id/password가 담긴 텍스트 파일에서 id와 password를 읽어내어 자동 로그인
 with open('idpass.txt','r',encoding='utf-8') as f:
     idnp=f.readlines()
 
@@ -13,16 +15,20 @@ id_input.send_keys(idnp[0])
 password_input = driver.find_element_by_css_selector('#login_pw')
 password_input.send_keys(idnp[1])
 
+#로그인 확인 버튼 클릭
 login_button = driver.find_element_by_css_selector('#login > form > button')
 login_button.click()
 time.sleep(3)
 
+#키워드로 찾기 페이지로 이동
 driver.get('https://ridibooks.com/keyword-finder/romance?order=review_cnt&page=1&set_id=1')
 time.sleep(3)
 
 def craw_code(path,page):
     code = []
     p = 1
+
+    #키워드 입력
     c_button = driver.find_element_by_css_selector(path)
     c_button.click()
     time.sleep(1)
@@ -31,16 +37,19 @@ def craw_code(path,page):
         l1='https://ridibooks.com/keyword-finder/romance?order=review_cnt&page='
         l2='&set_id=1'
 
+        #다음 페이지로 이동
         driver.get(l1 + str(p) + l2)
         time.sleep(3)
 
+        #북 코드 추출
         meta = driver.find_elements_by_css_selector("div.RSGBookMetadata")
         for c in meta:
             href=c.find_element_by_tag_name('a').get_attribute('href')
             code.append(href)
-
-
         p+=1
+
+
+    #이전 키워드 취소
     d_button = driver.find_element_by_css_selector('button.Keyword_DeleteButton')
     d_button.click()
     time.sleep(3)
@@ -54,24 +63,19 @@ if __name__ == "__main__":
         pagelist=[300,4,35]
 
         code=[]
+
+        #키워드- 단행본/연재중/연재완료 중 하나가 들어간 로맨스 소설 북코드 추출
         keyword_li=['#KeywordFinderRenewal > div.KeywordGroup > fieldset:nth-child(6) > div > div > div:nth-child(1) > ul > li:nth-child(3) > label',
                 '#KeywordFinderRenewal > div.KeywordGroup > fieldset:nth-child(6) > div > div > div:nth-child(1) > ul > li:nth-child(4) > label',
                 '#KeywordFinderRenewal > div.KeywordGroup > fieldset:nth-child(6) > div > div > div:nth-child(1) > ul > li:nth-child(5) > label']
 
         code += craw_code(keyword_li[0], pagelist[0])
-        print(len(code))
+        print(len(code))        #뽑아온 리스트 크기를 통해 몇개가 추출되었는지 확인
+
         for i in range(1,3):
             code+=craw_code(keyword_li[i],pagelist[i])
-            print(len(code))
+            print(len(code))        #뽑아온 리스트 크기를 통해 몇개가 추출되었는지 확인
 
-        f.writelines('\n'.join(code))
+        f.writelines('\n'.join(code))#txt파일에 쓰기
 
         driver.close()
-
-
-
-
-
-
-
-
--- crawler_ code/RIDI_crawler_review.py
+++ crawler_ code/RIDI_crawler_review.py
@@ -1,50 +1,110 @@
 from selenium import webdriver
+from bs4 import BeautifulSoup as bs
 import time
-import csv
 import pandas as pd
+import re
 
-
+#자동 로그인
 driver = webdriver.Chrome(r'.\chromedriver.exe')
 driver.get('https://ridibooks.com/account/login?return_url=https%3A%2F%2Fridibooks.com%2Fromance%2F')
 time.sleep(3)
 
+#id/password를 텍스트파일에서 읽어옴
 with open('idpass.txt','r',encoding='utf-8') as f:
     idnp=f.readlines()
 
+#id와 password입력
 id_input = driver.find_element_by_css_selector('#login_id')
 id_input.send_keys(idnp[0])
 password_input = driver.find_element_by_css_selector('#login_pw')
 password_input.send_keys(idnp[1])
 
+#로그인 버튼 클릭
 login_button = driver.find_element_by_css_selector('#login > form > button')
 login_button.click()
 time.sleep(3)
 
-driver.get('https://ridibooks.com/romance/')
-time.sleep(3)
-
 
 def crawling_info(infocode):#리뷰 크롤링
-    driver.get(infocode)
-    b1 = driver.find_element_by_css_selector('# review_list_section > div.rui_tab_and_order > '
-                                                   'ul.rui_tab_2.js_review_list_filter_wrapper > li:nth-child(1) > a')
+    driver.get(infocode)  # 북 코드를 통해 책 구매 페이지로 이동
+    time.sleep(3)
+
+
+    review = []
+    rate = []
+
+    # 구매자 리뷰 선택
+    b1 = driver.find_element_by_class_name('tab_list')
     b1.click()
     time.sleep(3)
 
-    b2=driver.find_elements_by_css_selector("")
-    time.sleep(3)
+    # 리뷰 끝까지 전부 보기
+    while (True):
+        try:
+            newb = driver.find_element_by_css_selector("#review_list_section > div.review_list_wrapper.js_review_list_wrapper.active > div.more_review_button_wrapper.js_more_review_button_wrapper > button")
+            newb.click()
+            time.sleep(3)
+        except:
+            break
+
+    #html가져오기
+    source = driver.page_source
+    html = bs(source, 'lxml')
+
+    # 평점과 리뷰 크롤링
+    info = html.select('li.review_list')
+
+    for i in info:
+        st = str(i.select_one('span.StarRate_IconFill'))
+        # 5점과 4점은 긍정(1), 3점은 애매모호(0) 1점과 2점은 부정(-1)으로 분류
+        if '100%' in st or '80%' in st:
+            r = '1'
+        elif '60%' in st:
+            r = '0'
+        else:
+            r = '-1'
+        # 리스트에 저장
+        rate.append(r)
+
+        f = str(i)
+
+        if '<span class="hidden">'in f:  #span class="hidden"이 있을 경우
+            ans = re.search(r'<span class="hidden">.+', f).group()
+            t = ans.replace('<span class="hidden">', '').replace('</span></p>', '').replace('<br/>','')
+        else:     # span class="hidden"이 없을 경우
+            ans = re.search(r'<p class="review_content js_review_content">.+', f).group()
+            t = ans.replace('<p class="review_content js_review_content">', '').replace('</p>', '').replace('<br/>','')
+
+        #리스트에 저장
+        review.append(t)
+
+    print('pass')
+    return zip(rate, review)  # 반환
 
 
+#메인
 if __name__ == "__main__":
-    df=pd.DataFrame
+    res = pd.DataFrame(columns=['rate', 'review'])
+    file=open('ridi_info.csv','w',encoding='utf-8')
+
     with open('ridi_code.txt','r',encoding='utf-8') as f:
-        c_line=f.readlines()
-    for code in c_line:
-        crawling_info(code.replace('\n',''))
+        c_line=f.readlines()# txt파일에 담긴 책 소개&구매 페이지 주소 가져오기
+
+    for code in c_line:#주소에서 별점과 리뷰 추출
+        review = crawling_info(code.replace('\n', '').replace('v2/Detail?id=', 'books/'))
+        df = pd.DataFrame(review, columns=['rate', 'review'])#반환된 데이터를 데이터프레임에 담아 저장
+        res = pd.concat([res, df], ignore_index=True,axis=0)
+        df.to_csv(r'ridi_info.csv', mode='w', index=False)
+        print(res)
+
+    print('=====최종=====')
+    print(res)
+
+    file.close()
+
+    # 드라이브 종료
+    driver.close()
+    driver.quit()
 
 
-
-    # KeywordFinderRenewal > div.KeywordGroup > fieldset:nth-child(6) > div > div > div:nth-child(1) > ul > li:nth-child(3) > label > input
-   # crawling_info(infocode)
-#https://ridibooks.com/keyword-finder/romance?order=recent&page=1&set_id=1
 
